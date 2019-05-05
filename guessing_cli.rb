6a9df3e579be2b99043dd2a7d33c2999c5fd9018
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-05 05:31:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,9 +9,7 @@ def run_guessing_game()
     puts "Goodbye!"
   elsif user_input == random_number
     puts "You guessed the correct number!"
-    run_guessing_game
   else
     puts "The computer guessed #{random_number}"
-    run_guessing_game
   end
 end
